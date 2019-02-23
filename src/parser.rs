38538877ec0196552edb56e541afe7b968bd7fba
Maria Kuklina <mkdirwtf@gmail.com>
2019-02-23 15:01:14 +0300

    Add parser module

---
+++ src/parser.rs
@@ -0,0 +1,284 @@
+/// `pest` is a parser crate.
+extern crate pest;
+
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(PartialEq, Eq, Debug)]
+pub enum Name {
+    /// Name could be a simple string
+    Plain(String),
+    /// Or a environment variable.
+    EnvVar(String),
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct SimpleCommand {
+    /// Name of the command.
+    name: ExtendName,
+    /// Its arguments.
+    args: Vec<ExtendName>,
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct ExportEnv {
+    /// Name of a variable.
+    name: String,
+    /// Its value.
+    value: String,
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub enum CommandType {
+    /// Execute a simple command (could contain environment variables).
+    Simple(SimpleCommand),
+    /// Export environment variable.
+    Env(ExportEnv),
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub enum Bind {
+    /// Bind two commands with pipe.
+    Pipe(Command),
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct Command {
+    /// Command itself.
+    cmd: CommandType,
+    /// Next binded command, if exists.
+    next: Option<Box<Bind>>,
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub enum ExtendName {
+    /// Simple name: identifier, environment variable or single quoted string.
+    Name(Name),
+    /// Double quoted string, which could include environment variable,
+    /// which we need to replace.
+    Extend(Vec<Name>),
+}
+
+#[derive(Parser)]
+// TODO: Is it a MAGICAL CONSTANT?
+#[grammar = "grammar.pest"]
+struct IdentParser;
+
+fn translate_rule_envvar(pair: pest::iterators::Pair<Rule>) -> Name {
+    let name = pair.into_inner().next().unwrap().as_str();
+    Name::EnvVar(name.to_string())
+}
+
+fn translate_rule_envset(pair: pest::iterators::Pair<Rule>) -> ExportEnv {
+    let mut pairs = pair.into_inner();
+
+    let name = pairs.next().unwrap().as_str();
+    let val = pairs.next().unwrap().as_str();
+
+    ExportEnv {
+        name: name.to_string(),
+        value: val.to_string(),
+    }
+}
+
+fn translate_rule_squote(pair: pest::iterators::Pair<Rule>) -> ExtendName {
+    ExtendName::Name(Name::Plain(pair.as_str().to_string()))
+}
+
+fn translate_rule_dquote(pair: pest::iterators::Pair<Rule>) -> ExtendName {
+    let pairs = pair.into_inner();
+    let mut extend = Vec::new();
+    for inner_pair in pairs {
+        let a = match inner_pair.as_rule() {
+            Rule::envvar => translate_rule_envvar(inner_pair),
+            Rule::ident_inside_sq_symb => Name::Plain(inner_pair.as_str().to_string()),
+            x => {
+                panic!(
+                    "Must not happen. Check grammar file. Error: {:?} {}",
+                    x,
+                    inner_pair.as_str()
+                )
+            }
+        };
+        extend.push(a);
+    }
+    ExtendName::Extend(extend)
+}
+
+fn translate_rule_name(pair: pest::iterators::Pair<Rule>) -> ExtendName {
+    let mut pairs = pair.into_inner();
+    let inner_pair = pairs.next().unwrap();
+    match inner_pair.as_rule() {
+        Rule::ident => ExtendName::Name(Name::Plain(inner_pair.as_str().to_string())),
+        Rule::envvar => ExtendName::Name(translate_rule_envvar(inner_pair)),
+        Rule::ident_inside_dq => translate_rule_dquote(inner_pair),
+        Rule::ident_inside_sq => translate_rule_squote(inner_pair),
+        x => {
+            panic!(
+                "Must not happen. Check grammar file. Error: {:?} {}",
+                x,
+                inner_pair.as_str()
+            )
+        }
+    }
+}
+
+fn translate_rule_option(pair: pest::iterators::Pair<Rule>) -> ExtendName {
+    let pairs = pair.into_inner().next().unwrap();
+    translate_rule_name(pairs)
+}
+
+fn translate_rule_single_cmd(pair: pest::iterators::Pair<Rule>) -> SimpleCommand {
+    let mut pairs = pair.into_inner();
+    let name = translate_rule_name(pairs.next().unwrap());
+    let mut opts = Vec::new();
+    for inner_pair in pairs {
+        match inner_pair.as_rule() {
+            Rule::option => {
+                let opt = translate_rule_option(inner_pair);
+                opts.push(opt);
+            }
+            x => {
+                panic!(
+                    "Must not happen. Check grammar file. Error: {:?} {}",
+                    x,
+                    inner_pair.as_str()
+                )
+            }
+        }
+    }
+    SimpleCommand {
+        name: name,
+        args: opts,
+    }
+}
+
+/// Translate inner `pest` representation of a command into our one.
+fn translate_rule_cmd(pair: pest::iterators::Pair<Rule>) -> Command {
+    let mut pairs = pair.into_inner();
+    let inner_pair = pairs.next().unwrap();
+    let cmd = match inner_pair.as_rule() {
+        Rule::single_cmd => CommandType::Simple(translate_rule_single_cmd(inner_pair)),
+        Rule::envset => CommandType::Env(translate_rule_envset(inner_pair)),
+        x => {
+            panic!(
+                "Must not happen. Check grammar file. Error: {:?} {}",
+                x,
+                inner_pair.as_str()
+            )
+        }
+    };
+
+    let next_pair = pairs.next().unwrap();
+    let next_cmd = match next_pair.as_rule() {
+        Rule::cmd => Some(Box::new(Bind::Pipe(translate_rule_cmd(next_pair)))),
+        Rule::EOI => None,
+        x => panic!("Must not happe: {:?} {}", x, next_pair.as_str()),
+    };
+
+    Command {
+        cmd: cmd,
+        next: next_cmd,
+    }
+}
+
+/// Parse string command into inner representation of a command structure.
+fn parse_cmd(cmd: String) -> Option<Command> {
+    match IdentParser::parse(Rule::cmd, cmd.as_str()) {
+        Ok(mut pairs) => Some(translate_rule_cmd(pairs.next().unwrap())),
+        _ => None,
+    }
+}
+
+
+#[cfg(test)]
+mod test_parser {
+    use super::*;
+
+    use super::ExtendName::*;
+    use super::Name::*;
+
+    #[test]
+    fn test_simple() {
+        let tcmd = parse_cmd("echo 1 2".to_string()).unwrap();
+        let dcmd = Command {
+            cmd: CommandType::Simple(SimpleCommand {
+                name: Name(Plain("echo".to_string())),
+                args: vec![Name(Plain("1".to_string())), Name(Plain("2".to_string()))],
+            }),
+            next: None,
+        };
+        assert_eq!(tcmd, dcmd);
+    }
+
+    #[test]
+    fn test_simple_env() {
+        let tcmd = parse_cmd("echo $var".to_string()).unwrap();
+        let dcmd = Command {
+            cmd: CommandType::Simple(SimpleCommand {
+                name: Name(Plain("echo".to_string())),
+                args: vec![Name(EnvVar("var".to_string()))],
+            }),
+            next: None,
+        };
+        assert_eq!(tcmd, dcmd);
+    }
+
+    #[test]
+    fn test_simpl_sq() {
+        let tcmd = parse_cmd("echo '$var'".to_string()).unwrap();
+        let dcmd = Command {
+            cmd: CommandType::Simple(SimpleCommand {
+                name: Name(Plain("echo".to_string())),
+                args: vec![Name(Plain("$var".to_string()))],
+            }),
+            next: None,
+        };
+        assert_eq!(tcmd, dcmd);
+    }
+
+    #[test]
+    fn test_simpl_dq() {
+        let tcmd = parse_cmd("echo \"$var\"".to_string()).unwrap();
+        let dcmd = Command {
+            cmd: CommandType::Simple(SimpleCommand {
+                name: Name(Plain("echo".to_string())),
+                args: vec![Extend(vec![EnvVar("var".to_string())])],
+            }),
+            next: None,
+        };
+        assert_eq!(tcmd, dcmd);
+    }
+
+    #[test]
+    fn test_env() {
+        let tcmd = parse_cmd("var=10".to_string()).unwrap();
+        let dcmd = Command {
+            cmd: CommandType::Env(ExportEnv {
+                name: "var".to_string(),
+                value: "10".to_string(),
+            }),
+            next: None,
+        };
+        assert_eq!(tcmd, dcmd);
+    }
+
+    #[test]
+    fn test_pipe() {
+        let tcmd = parse_cmd("echo 1 2 | wc".to_string()).unwrap();
+        let dcmd = Command {
+            cmd: CommandType::Simple(SimpleCommand {
+                name: Name(Plain("echo".to_string())),
+                args: vec![Name(Plain("1".to_string())), Name(Plain("2".to_string()))],
+            }),
+            next: Some(Box::new(Bind::Pipe(Command {
+                cmd: CommandType::Simple(SimpleCommand {
+                    name: Name(Plain("wc".to_string())),
+                    args: vec![],
+                }),
+                next: None,
+            }))),
+        };
+        assert_eq!(tcmd, dcmd);
+    }
+}
