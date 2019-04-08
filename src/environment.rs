97421fb852210fc12a9c6a1d0596cec9b0bc82d5
Maria Kuklina <mkdirwtf@gmail.com>
2019-04-08 11:57:45 +0300

    Add comments

--- src/environment.rs
+++ src/environment.rs
@@ -1,6 +1,10 @@
 use std::collections::HashMap;
 
 /// Local environement.
+///
+/// Used only for string interpolation. We dont' export
+/// these variables, so they're not in the environement
+/// of subsequently executed commands.
 pub struct Env(HashMap<String, String>);
 
 impl Env {
@@ -8,10 +12,12 @@ impl Env {
         Env(HashMap::new())
     }
 
+    /// Add a new variable with given `name` and `value`.
     pub fn add(&mut self, name: String, value: String) {
         self.0.insert(name, value);
     }
 
+    /// Get variable with given `name`.
     pub fn get(&self, name: &String) -> Option<String> {
         self.0.get(name).cloned()
     }
