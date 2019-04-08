cbf77dc1e40ad1933eb1840dd0d1d02f8b320fef
Maria Kuklina <mkdirwtf@gmail.com>
2019-04-08 12:11:23 +0300

    Add comments for builtin module

--- src/builtins/cat.rs
+++ src/builtins/cat.rs
@@ -2,10 +2,17 @@ use super::common::get_content;
 use crate::error::ShellError;
 use crate::shell::ShellResult;
 
+/// Print content of the file given in the first argument.
+/// If no argument given reads from pipe.
+///
+/// Doesn't read from stdin.
+/// Doesn't actualy concatenate files.
 pub fn cat(args: &Vec<String>, msg: Option<String>) -> ShellResult<String> {
     if args.len() != 0 {
+        // It's okay if we get more than one argument. Just take the first.
         return get_content(&args[0]);
     }
+    // Or try to read from pipe.
     if let Some(content) = msg {
         return ShellResult::Ok(content);
     }
--- src/builtins/echo.rs
+++ src/builtins/echo.rs
@@ -1,5 +1,8 @@
 use crate::shell::ShellResult;
 
+/// Print given arguments.
+///
+/// Doesn't read from pipe.
 pub fn echo(args: &Vec<String>, _msg: Option<String>) -> ShellResult<String> {
     ShellResult::Ok(args.join(" "))
 }
