f3dce2586e308cd3951a610968fa7a8c63c16717
akhoroshev <arthoroshev@gmail.com>
2019-06-17 16:36:42 +0300

    Fix bugs and add tests

--- src/builtins/cd.rs
+++ src/builtins/cd.rs
@@ -4,14 +4,35 @@ use crate::shell::ShellResult;
 use std::env;
 use std::path::PathBuf;
 
+/// Change working directory to specfied
 pub fn cd(args: &Vec<String>, _stdin: Option<String>) -> ShellResult<String> {
     if args.len() > 1 {
-        return ShellResult::Err(ShellError::Error("Too many args"))
+        return ShellResult::Err(ShellError::Error("cd: too many arguments"))
     } else if args.len() == 1 {
         let srcdir = PathBuf::from(&args[0]);
         if !env::set_current_dir(srcdir).is_ok() {
-            return ShellResult::Err(ShellError::Error("Error"))
+            return ShellResult::Err(ShellError::Error("cd: no such file or directory"))
         }
+    } else {
+        return ShellResult::Err(ShellError::Error("cd: to few arguments"))
     }
-    ShellResult::Empty
+    ShellResult::Ok("".to_string())
+}
+
+
+#[cfg(test)]
+mod test_cd {
+    use super::*;
+
+    #[test]
+    fn test_up_down() {
+        let cur_dir = env::current_dir().unwrap();
+        let args = vec!["..".to_string()];
+        cd(&args, Option::None);
+        
+        let new_args = vec![cur_dir.into_os_string().into_string().unwrap()];
+        cd(&new_args, Option::None);
+
+        assert_eq!(new_args[0], env::current_dir().unwrap().into_os_string().into_string().unwrap());
+    }    
 }
--- src/builtins/ls.rs
+++ src/builtins/ls.rs
@@ -6,20 +6,22 @@ use std::path::PathBuf;
 use std::fs;
 use std::path::Path;
 
+/// Print folder content
 pub fn ls(args: &Vec<String>, _stdin: Option<String>) -> ShellResult<String> {
     if args.len() > 1 {
-        return ShellResult::Err(ShellError::Error("Too many args"));
+        return ShellResult::Err(ShellError::Error("ls: too many arguments"));
     } else if args.len() == 1 {
         let dir = PathBuf::from(&args[0]);
         return ShellResult::Ok(visit_dirs(&dir).join("\n"));
     } else {
         match env::current_dir() {
             Ok(dir) => ShellResult::Ok(visit_dirs(&dir).join("\n")),
-            Err(_err) => ShellResult::Err(ShellError::Error("Error")),
+            Err(_err) => ShellResult::Err(ShellError::Error("ls: could not get current dirrectory")),
         }
     }
 }
 
+/// Function returns all filenames in given path
 fn visit_dirs(dir: &Path) -> Vec<String> {
     let mut result = vec![];
     if dir.is_dir() {
@@ -34,3 +36,25 @@ fn visit_dirs(dir: &Path) -> Vec<String> {
     return result;
 }
 
+#[cfg(test)]
+mod test_ls {
+    use super::*;
+
+    #[test]
+    fn test_content() {
+        match ls(&vec![".".to_string()], Option::None) {
+            ShellResult::Ok(res) => assert!(res.len() > 0),
+            ShellResult::Err(_) => assert!(false)
+        };
+
+        match ls(&vec![], Option::None) {
+            ShellResult::Ok(res) => assert!(res.len() > 0),
+            ShellResult::Err(_) => assert!(false)
+        };
+
+        match ls(&vec!["/%@$#lstestfoldernotexist@#@%)_/".to_string()], Option::None) {
+            ShellResult::Ok(res) => assert_eq!(0, res.len()),
+            ShellResult::Err(_) => assert!(false)
+        };
+    }    
+}
